0658d8ed3169ed97d855dc2d602d17c63d127834
liangwei0101 <liangwei0101@outlook.com>
2020-06-05 15:23:48 +0800

    增加注释

--- src/common/decorator/customize.ts
+++ src/common/decorator/customize.ts
@@ -6,6 +6,6 @@ import { SetMetadata } from '@nestjs/common'
 export const NoAuth = () => SetMetadata('no-auth', true);
 
 /**
-* 接口角色
+* 某个角色能访问
 */
 export const Roles = (...roles: string[]) => SetMetadata('roles', roles);
