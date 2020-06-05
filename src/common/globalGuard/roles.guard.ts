ceab6f3701ecfc2a7131d40c621de8dbb590db4c
liangwei0101 <liangwei0101@outlook.com>
2020-06-05 15:10:11 +0800

    增加角色设置

--- src/common/globalGuard/roles.guard.ts
+++ src/common/globalGuard/roles.guard.ts
@@ -1,20 +1,21 @@
-import { CanActivate, ExecutionContext } from "@nestjs/common";
-import { Reflector } from "@nestjs/core";
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { Reflector } from '@nestjs/core';
 
+@Injectable()
 export class RolesGuard implements CanActivate {
   constructor(private readonly reflector: Reflector) { }
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
 
-  canActivate(context: ExecutionContext): boolean {
     const roles = this.reflector.get<string[]>('roles', context.getHandler());
-    
-    // 无权限标志，所有角色都能访问
+    // 无角色注释时，api任务权限都能访问
     if (!roles) {
       return true;
     }
-
     const request = context.switchToHttp().getRequest();
     const user = request.user;
     const hasRole = () => user.roles.some((role: string) => roles.indexOf(role) > -1);
+    console.log('hasRole', hasRole);
 
     return user && user.roles && hasRole();
   }
