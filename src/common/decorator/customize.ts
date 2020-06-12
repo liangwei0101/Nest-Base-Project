05799e45f14583c051348a490b533800947d86f8
liangwei0101 <liangwei0101@outlook.com>
2020-06-12 19:15:47 +0800

    增加登录接口

--- src/common/decorator/customize.ts
+++ src/common/decorator/customize.ts
@@ -6,6 +6,11 @@ import { SetMetadata, createParamDecorator, ExecutionContext } from '@nestjs/com
 export const NoAuth = () => SetMetadata('no-auth', true);
 
 /**
+* 登录认证
+*/
+export const LoginAuth = () => SetMetadata('login-auth', true);
+
+/**
 * 某个角色能访问
 */
 export const Roles = (...roles: string[]) => SetMetadata('roles', roles);
--- src/common/globalGuard/auth.guard.ts
+++ src/common/globalGuard/auth.guard.ts
@@ -9,16 +9,24 @@ import { LocalAuthGuard } from '../specialModules/auth/guards/auth.local.guard';
 export class GlobalAuthGuard implements CanActivate {
   constructor(private readonly reflector: Reflector) { }
   canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+
+    // 获取登录的注解
+    const loginAuth = this.reflector.get<boolean>('login-auth', context.getHandler());
+
     // 在这里取metadata中的no-auth，得到的会是一个bool
     const noAuth = this.reflector.get<boolean>('no-auth', context.getHandler());
-    const guard = GlobalAuthGuard.getAuthGuard(noAuth);
+    if (noAuth) {
+      return true;
+    }
+
+    const guard = GlobalAuthGuard.getAuthGuard(loginAuth);
     // 执行所选策略Guard的canActivate方法
     return guard.canActivate(context);
   }
 
   // 根据NoAuth的t/f选择合适的策略Guard
-  private static getAuthGuard(noAuth: boolean): IAuthGuard {
-    if (noAuth) {
+  private static getAuthGuard(loginAuth: boolean): IAuthGuard {
+    if (loginAuth) {
       return new LocalAuthGuard();
     } else {
       return new JwtAuthGuard();
